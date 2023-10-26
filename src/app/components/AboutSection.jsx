c46139a1436d2937f1beba16305c4d0ed5402c6b
GilangMF11 <gilangfahmi20@gmail.com>
2023-10-26 13:05:30 +0700

    Portofolio next js

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,99 +1,88 @@
-"use client";
-import React, { useTransition, useState } from "react";
-import Image from "next/image";
-import TabButton from "./TabButton";
+'use client';
+import React, { useTransition, useState} from 'react'
+import Image from 'next/image';
+import TabButton from './TabButton';
+
 
 const TAB_DATA = [
   {
-    title: "Skills",
-    id: "skills",
+    id: "Keterampilan",
+    title: "Keterampilan",
     content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
+      <ul className='list-disc pl-2'>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>Codeigniter</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>Tailwind</li>
+        <li>Javascript</li>
+        <li>Node JS</li>
       </ul>
-    ),
+    )
   },
   {
-    title: "Education",
-    id: "education",
+    id: "Pendidikan",
+    title: "Pendidikan",
     content: (
-      <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+      <ul className='list-disc pl-2'>
+        <li>Universitas Amikom Purwokerto - 2023</li>
       </ul>
-    ),
+    )
   },
   {
-    title: "Certifications",
-    id: "certifications",
+    id: "Sertifikat",
+    title: "Sertifikat",
     content: (
-      <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+      <ul className='list-disc pl-2'>
+        <li>Bahasa Inggris - Amikom Purwokerto</li>
+        <li>Digital Forensec - Academy Forensec Curam Victory</li>
+        <li>Javascript - Dicoding Indonesia</li>
       </ul>
-    ),
-  },
-];
-
-const AboutSection = () => {
-  const [tab, setTab] = useState("skills");
+    )
+  }
+  
+]
+function AboutSection() {
+  const [tab, setTab] = useState("Keterampilan");
   const [isPending, startTransition] = useTransition();
 
-  const handleTabChange = (id) => {
-    startTransition(() => {
+  const hanldeTabChange = (id) => {
+    startTransition (() => {
       setTab(id);
-    });
-  };
+    })  
+  }
 
   return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
+    <section className='text-white'>
+      <div className='grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16'>
+          <Image className='rounded' src="/images/about-image.png" width={500} height={500} alt="about"/>
+          <div className='mt-4 md:mt-0 text-left flex flex-col  h-full'>
+            <h2 className='text-4xl font-bold text-white mb-4'>Tentang Saya</h2>
+            <p className='text-base lg:text-lg justify-items-start'>Perkenalkan nama saya Gilang Miftakhul Fahmi lulusan teknik informatika Universitas Amikom Purwokerto pada bulan maret 2023, Sekarang saya berfokus pada bidang teknologi tentunya pada pemrograman baik website dengan pengalaman kurang lebih 1 Tahun. Pengalaman saya pada aplikasi yang saya buat merupakan teknologi dari HTML, CSS, Javascript maupun menggunakan framework PHP seperti Laravel dan Codeigniter, pada Javascript seperti Node.js ataupun menggunakan framework CSS Seperti Tailwind dan Boostraps 5. </p>
+          <div className='flex flex-row justify-start mt-8'>
+              <TabButton selecTab={() => hanldeTabChange('Keterampilan')} active={tab === "Keterampilan"}>
+                {" "}
+                Keterampilan {" "}
+                </TabButton>
+
+                <TabButton selecTab={() => hanldeTabChange('Pendidikan')} active={tab === "Pendidikan"}>
+                {" "}
+                Pendidikan {" "}
+                </TabButton>
+
+                <TabButton selecTab={() => hanldeTabChange('Sertifikat')} active={tab === "Sertifikat"}>
+                {" "}
+                Sertifikat {" "}
+                </TabButton>
+
           </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
+          <div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}</div>
           </div>
-        </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
 export default AboutSection;
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,80 +2,51 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
-import Link from "next/link";
 
 const HeroSection = () => {
-  return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
-            </span>
-            <br></br>
-            <TypeAnimation
-              sequence={[
-                "Judy",
-                1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
-          </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptatum.
-          </p>
-          <div>
-            <Link
-              href="/contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
-            <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
-        </motion.div>
-      </div>
+    return (
+    <section>
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text:text-left">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Hallo, Saya {" "}</span> 
+                    <br/>
+                    <TypeAnimation sequence={[
+                        "Gilang Miftakhul Fahmi", 
+                        1000,
+                        "Web Developer",
+                        1000,
+                        "Mobile Developer",
+                        1000,
+                        "Freelancer",
+                        1000
+                    ]}
+                    wrapper="span"
+                    repeat={Infinity}
+                    speed={50}
+                    />
+                </h1>
+                <p className="text-[#ADB7BE] sm:text-lg text-lg mb-6 lg:text-xl">
+                    Website Portofolio Next JS + Tailwind
+                </p>
+                <div>
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white">Hire Me</button>
+                    <button className="px-6 py-3 w-full sm:w-fit  rounded-full  bg-black hover:bg-gray-50 text-white hover:text-black border border-white mt-3">Download CV</button>
+                </div>
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image 
+                    src={"/images/Gilang.png"} 
+                    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    width={150} 
+                    height={150}
+                    />
+                </div>
+            </div>
+        </div>
     </section>
-  );
+    );
 };
 
 export default HeroSection;
